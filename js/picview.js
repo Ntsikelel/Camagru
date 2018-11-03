9b1ec2024e0610d9fa4cd1e6b9cb50addfd0bed6
Ntsikelelo METSEEME <nmetseem@e5r5p8.wethinkcode.co.za>
2018-11-03 20:17:41 +0200

    better

--- js/picview.js
+++ js/picview.js
@@ -1,9 +1,14 @@
-function open_camera()
+  
+    var can; 
+    var cont;
+    var norep = false;
+   function open_camera()
 {
     var cam = document.getElementById('vid');
+    can = document.getElementById('canv');
     var btn = document.getElementById('open_cam');
     btn.parentNode.removeChild(btn);
-    var can = document.getElementById('canv');
+ 
     var cont = can.getContext('2d');
     navigator.mediaDevices.getUserMedia({video:true}).then(function(stream){cam.src = window.URL.createObjectURL(stream); cam.play();}); 
     
@@ -12,5 +17,35 @@ function open_camera()
         cont.drawImage(cam,0,0,500,500);
         var data = can.toDataURL();
         alert(data);
+
+    });
+}
+function view_img(num)
+{
+    alert('smp'+num);
+    
+    can = document.getElementById('canv');
+    var cont = can.getContext('2d');
+    var item = document.getElementById('smp'+ num);
+    cont.drawImage(item,400,300,100,100);
+    if(norep === false){
+    var btn = document.createElement("BUTTON");
+    btn.id = "save";
+    btn.innerHTML =' <img src= "save_image.jpg" width ="100" height = "100" style= "border-radius: 50%;"/>';
+    btn.width = 100;
+    btn.height = 100;
+    btn.style.background = "transparent";
+    btn.style.border = "none";
+    var text = document.createTextNode("Save");
+    btn.appendChild(text);
+    
+        document.getElementById('cam').appendChild(btn);
+    norep = true;
+}
+    document.getElementById('save').addEventListener("click",function()
+    {
+        var data = can.toDataURL();
+        alert(data);
+        // "<?php  header('Location: http://localhost:8080/Camagru/login.php');?> ";
     });
 }
--- view.php
+++ view.php
@@ -14,16 +14,17 @@
 <body>
     <div id ="cam">
     <video id= "vid" width="600" height = "600" style= "background : black"></video> 
+      <script src= "js/picview.js"></script>
     <table>
         <tr id="smp_img">
-            <td><img src="smp1.png" width="100" height = "100"></td>
-            <td><img src="smp2.png" width="100" height = "100"></td>
-            <td><img src="smp1.png" width="100" height = "100"></td>
-            <td><img src="smp2.png" width="100" height = "100"></td>
-            <td><img src="smp1.png" width="100" height = "100"></td>
+            <td><img src="smp1.png" width="100" height = "100" onclick="view_img(1)" id = "smp1"></td>
+            <td><img src="smp2.png" width="100" height = "100" onclick="view_img(2)" id = "smp2"></td>
+            <td><img src="smp1.png" width="100" height = "100" onclick="view_img(3)" id = "smp3"></td>
+            <td><img src="smp2.png" width="100" height = "100" onclick="view_img(4)" id = "smp4"></td>
+            <td><img src="smp1.png" width="100" height = "100" onclick="view_img(5)" id = "smp5"></td>
         </tr>
     </table>
-    <script src= "js/picview.js"></script>
+  
     <img src = "camera1.jpg" onclick= "open_camera()" id = "open_cam" width="60" height = "60">
     <img src = "camera1.jpg" id = "cap" width="60" height = "60">
     <canvas id = "canv" width="6400" height = "400" style= "background: transparent"></canvas>
